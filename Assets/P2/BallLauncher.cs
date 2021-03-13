e176ca423f0ace6fc5b5b2e5aa462cda01f5a075
TianRy1337 <Ryan55014TianZhang@gmail.com>
2021-03-13 20:25:58 +0800

    P2
    Ball Problem

---
+++ Assets/P2/BallLauncher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour
+{
+    public Rigidbody ball;
+    public Transform target;
+
+    public float h = 25;
+    public float gravity = -18;
+    private void Start() {
+        ball.useGravity = false;
+    }
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Launch();
+        }
+        
+    }
+    void Launch()
+    {
+        Physics.gravity = Vector3.up * gravity;
+        ball.useGravity =true;
+        ball.velocity = CalculateLaunchVelocity();
+        print(CalculateLaunchVelocity());
+    }
+
+    Vector3 CalculateLaunchVelocity()
+    {
+        float displacementY = target.position.y - ball.position.y;
+        Vector3 displacementXZ = new Vector3(target.position.x - ball.position.x, 0, target.position.z - ball.position.z);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * h);
+        Vector3 velocityZ = displacementXZ / (Mathf.Sqrt(-2 * h / gravity) + Mathf.Sqrt(2 * (displacementY - h) / gravity));
+        return velocityZ + velocityY;
+    }
+}
